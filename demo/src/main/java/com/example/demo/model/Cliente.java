d0b95fa972ecbf73af5a1be2dd7280ed2a8d9057
SamuelSilva15 <Silvamsamuel15@gmail.com>
2022-09-14 12:06:46 -0300

    Iniciando projeto

---
+++ demo/src/main/java/com/example/demo/model/Cliente.java
@@ -0,0 +1,62 @@
+package com.example.demo.model;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Cliente {
+    private String nome;
+    private SimpleDateFormat dataDeNascimento = new SimpleDateFormat("dd/MM/yyyy");
+    private String sexo;
+    private String problemasDeSaude;
+    private String dataDeCriação = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy, HH:mm:ss"));;
+    private String dataDeAtualização;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public SimpleDateFormat getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(SimpleDateFormat dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getProblemasDeSaude() {
+        return problemasDeSaude;
+    }
+
+    public void setProblemasDeSaude(String problemasDeSaude) {
+        this.problemasDeSaude = problemasDeSaude;
+    }
+
+    public String getDataDeCriação() {
+        return dataDeCriação;
+    }
+
+    public void setDataDeCriação(String dataDeCriação) {
+        this.dataDeCriação = dataDeCriação;
+    }
+
+    public String getDataDeAtualização() {
+        return dataDeAtualização;
+    }
+
+    public void setDataDeAtualização(String dataDeAtualização) {
+        this.dataDeAtualização = dataDeAtualização;
+    }
+}
